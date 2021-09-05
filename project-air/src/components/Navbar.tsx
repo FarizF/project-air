728c579881b795d4fae2281bcab0d9c4e33ccd6a
F.D.B. Fakkel <fariz.fakkel@hva.nl>
2021-09-05 18:55:23 +0200

    Added basic navbar logic for loggedIn and !loggedIn users

--- project-air/src/components/Navbar.tsx
+++ project-air/src/components/Navbar.tsx
@@ -1,12 +1,26 @@
+let loggedIn: boolean;
+
+const homeButtonList: JSX.Element =
+	<ul>
+		<li className="nav-button"></li>
+		<li className="nav-button"></li>
+		<li className="nav-button"></li>
+		<li className="nav-button user" id="login-register">Log On</li>
+	</ul>;
+
+const inGameButtonList: JSX.Element =
+	<ul>
+		<li className="nav-button"></li>
+		<li className="nav-button"></li>
+		<li className="nav-button"></li>
+		<li className="nav-button user" id="login-off-profile">Log Off</li>
+	</ul>
+
 export default function Navbar() {
 	return (
 		<header>
-			<nav>
-				<ul>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
+			<nav id="navbar">
+				{loggedIn ? inGameButtonList : homeButtonList}
 			</nav>
 		</header>
 	)
--- project-air/src/components/home.tsx
+++ project-air/src/components/home.tsx
@@ -1,3 +1,4 @@
+import './Home.scss';
 import logo from '../logo.svg';
 import Navbar from './Navbar';
 
@@ -5,21 +6,8 @@ export default function Home() {
 	return (
 		<>
 			<Navbar/>
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.tsx</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
+			<div id="Home">
+				
 			</div>
 		</>
 	);
